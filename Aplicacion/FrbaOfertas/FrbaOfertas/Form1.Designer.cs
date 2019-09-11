efd265c198920998fcfc213c566114a7936edf8d
afondevilarevelli <antonny.42@gmail.com>
2019-09-11 18:53:17 -0300

    Pantalla inicio de sesion + ajuste tamaño pantallas

--- Aplicacion/FrbaOfertas/FrbaOfertas/Form1.Designer.cs
+++ Aplicacion/FrbaOfertas/FrbaOfertas/Form1.Designer.cs
@@ -28,12 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.error_message = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ingresar_btn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.user = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.error_message);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.ingresar_btn);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.password);
+            this.panel1.Controls.Add(this.user);
+            this.panel1.Location = new System.Drawing.Point(-2, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(780, 444);
+            this.panel1.TabIndex = 0;
+            // 
+            // error_message
+            // 
+            this.error_message.AutoSize = true;
+            this.error_message.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.error_message.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(97)))), ((int)(((byte)(97)))));
+            this.error_message.Location = new System.Drawing.Point(480, 287);
+            this.error_message.Name = "error_message";
+            this.error_message.Size = new System.Drawing.Size(191, 25);
+            this.error_message.TabIndex = 7;
+            this.error_message.Text = "Error al iniciar sesión";
+            this.error_message.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(292, 356);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 20);
+            this.label4.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(571, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(197, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Inicio de sesión";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // ingresar_btn
+            // 
+            this.ingresar_btn.Location = new System.Drawing.Point(296, 274);
+            this.ingresar_btn.Name = "ingresar_btn";
+            this.ingresar_btn.Size = new System.Drawing.Size(168, 47);
+            this.ingresar_btn.TabIndex = 4;
+            this.ingresar_btn.Text = "Ingresar";
+            this.ingresar_btn.UseVisualStyleBackColor = true;
+            this.ingresar_btn.Click += new System.EventHandler(this.ingresar_btn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(245, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Tag = "jkhjk";
+            this.label2.Text = "Ingrese contraseña";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(245, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Tag = "jkhjk";
+            this.label1.Text = "Ingrese usuario";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(249, 220);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(260, 26);
+            this.password.TabIndex = 1;
+            this.password.UseSystemPasswordChar = true;
+            // 
+            // user
+            // 
+            this.user.Location = new System.Drawing.Point(249, 141);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(260, 26);
+            this.user.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(778, 444);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ofertas!";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ingresar_btn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label error_message;
     }
 }
 
--- Aplicacion/FrbaOfertas/FrbaOfertas/Form1.cs
+++ Aplicacion/FrbaOfertas/FrbaOfertas/Form1.cs
@@ -16,5 +16,34 @@ namespace FrbaOfertas
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ingresar_btn_Click(object sender, EventArgs e)
+        {
+            string username = user.Text;
+            string pass = password.Text;
+
+            //Chequear si existe
+            bool isValidated = username == "antonio" && pass == "1234";
+
+            if (isValidated)
+            {
+                this.Hide();
+                new AbmCliente.Form1().Show();
+            }
+            else
+            {
+                error_message.Visible = true;
+            }
+        }
     }
 }
