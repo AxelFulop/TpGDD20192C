29d830365ec43cc0a40741b4dfc3c67f3fa96686
afondevilarevelli <antonny.42@gmail.com>
2019-11-14 15:54:43 -0300

    AbmCliente y AbmRol con botones eliminar y editar para cada row

--- Aplicacion/FrbaOfertas/FrbaOfertas/AbmProveedor/AbmProveedor.cs
+++ Aplicacion/FrbaOfertas/FrbaOfertas/AbmProveedor/AbmProveedor.cs
@@ -28,7 +28,19 @@ namespace FrbaOfertas.AbmProveedor
 
             ConexionBD.Conexion conection = new ConexionBD.Conexion().getInstance();
             proveedores = conection.selectReturnMultiplyRowsByQuery(query);
-            grid.DataSource = proveedores; 
+            grid.DataSource = proveedores;
+
+            DataGridViewButtonColumn colEditar = new DataGridViewButtonColumn();
+            colEditar.UseColumnTextForButtonValue = true;
+            colEditar.Text = "Editar";
+            colEditar.Name = "";
+            grid.Columns.Add(colEditar);
+
+            DataGridViewButtonColumn colEliminar = new DataGridViewButtonColumn();
+            colEliminar.UseColumnTextForButtonValue = true;
+            colEliminar.Text = "Eliminar";
+            colEliminar.Name = "";
+            grid.Columns.Add(colEliminar);
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -38,17 +50,38 @@ namespace FrbaOfertas.AbmProveedor
             if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn &&
                 e.RowIndex >= 0 && e.RowIndex < grid.Rows.Count - 1)
             {
-                if (e.ColumnIndex == 8) //Editar
+                if (e.ColumnIndex == 0) //Editar
                 {
-                    Dictionary<string, string> datos = ajustarDatosRow(grid.CurrentRow.Cells);
+                    DataRow row = (grid.CurrentRow.DataBoundItem as DataRowView).Row;
+                    Dictionary<string, string> datos = ajustarDatosRow(row);
                     this.Hide();
                     new Editar(datos).Show();
                 }
-                else if (e.ColumnIndex == 9) //Eliminar
+                else if (e.ColumnIndex == 1) //Eliminar
                 {
-                    Dictionary<string, string> datos = ajustarDatosRow(grid.CurrentRow.Cells);
-                    eliminar(datos);
-                    this.Refresh();
+                    DataRow row = (grid.CurrentRow.DataBoundItem as DataRowView).Row;
+                    Dictionary<string, string> datos = ajustarDatosRow(row);
+                    DialogResult result = MessageBox.Show("¿Desea eliminar el proveedor '" + datos["razonSocial"] + "'?",
+                    "",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+                    if (result == DialogResult.Yes)
+                    {
+                        try
+                        {
+                            eliminar(datos);
+                            this.Refresh();
+                            MessageBox.Show("Proveedor eliminado correctamente", "",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+                        }
+                        catch (Exception)
+                        {
+                            MessageBox.Show("Error al eliminar el proveedor", "",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                        }
+                    }
                 }
             }
         }
@@ -73,18 +106,18 @@ namespace FrbaOfertas.AbmProveedor
             grid.DataSource = proveedores;
         }
 
-        private Dictionary<string, string> ajustarDatosRow(DataGridViewCellCollection row)
+        private Dictionary<string, string> ajustarDatosRow(DataRow row)
         {
             Dictionary<string, string> datosRow = new Dictionary<string, string>();
 
-            datosRow["razonSocial"] = row["RazonColumn"].Value.ToString();
-            datosRow["mail"] = row["MailColumn"].Value.ToString();
-            datosRow["telefono"] = row["TelefonoColumn"].Value.ToString();
-            datosRow["direccion"] = row["DireccionColumn"].Value.ToString();
-            datosRow["codigoPostal"] = row["CPColumn"].Value.ToString();
-            datosRow["cuit"] = row["CuitColumn"].Value.ToString();
-            datosRow["rubro"] = row["RubroColumn"].Value.ToString();
-            datosRow["contacto"] = row["ContactoColumn"].Value.ToString();
+            datosRow["razonSocial"] = row.ItemArray[0].ToString();
+            datosRow["mail"] = row.ItemArray[1].ToString();
+            datosRow["telefono"] = row.ItemArray[2].ToString();
+            datosRow["direccion"] = row.ItemArray[3].ToString();
+            datosRow["codigoPostal"] = row.ItemArray[4].ToString();
+            datosRow["cuit"] = row.ItemArray[5].ToString();
+            datosRow["rubro"] = row.ItemArray[6].ToString();
+            datosRow["contacto"] = "";//row.ItemArray[7].ToString();
 
             return datosRow;
         }
