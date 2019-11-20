8bb2e803d111d8b504f1da941c66207114635f1c
afondevilarevelli <antonny.42@gmail.com>
2019-11-20 19:14:11 -0300

    Filtrar ofertas + otras cositas

--- Aplicacion/FrbaOfertas/FrbaOfertas/ComprarOferta/ComprarOfertaABM.Designer.cs
+++ Aplicacion/FrbaOfertas/FrbaOfertas/ComprarOferta/ComprarOfertaABM.Designer.cs
@@ -30,15 +30,15 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.grid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(695, 8);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(1042, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 36);
+            this.button1.Size = new System.Drawing.Size(123, 55);
             this.button1.TabIndex = 0;
             this.button1.Text = "Volver";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,29 +47,42 @@
             // grid
             // 
             this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grid.Location = new System.Drawing.Point(12, 75);
+            this.grid.Location = new System.Drawing.Point(10, 115);
+            this.grid.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.grid.Name = "grid";
-            this.grid.Size = new System.Drawing.Size(763, 337);
+            this.grid.Size = new System.Drawing.Size(1149, 497);
             this.grid.TabIndex = 1;
+            this.grid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grid_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(451, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Seleccione la oferta a comprar";
             // 
             // ComprarOfertaABM
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(787, 424);
+            this.ClientSize = new System.Drawing.Size(1172, 626);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.grid);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(803, 463);
+            this.MaximumSize = new System.Drawing.Size(1194, 682);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(685, 463);
+            this.MinimumSize = new System.Drawing.Size(1016, 682);
             this.Name = "ComprarOfertaABM";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Comprar ofertas";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -77,5 +90,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Aplicacion/FrbaOfertas/FrbaOfertas/ComprarOferta/ComprarOfertaABM.cs
+++ Aplicacion/FrbaOfertas/FrbaOfertas/ComprarOferta/ComprarOfertaABM.cs
@@ -28,8 +28,9 @@ namespace FrbaOfertas.ComprarOferta
 
         private void cargarOfertas()
         {
-            string query = "SELECT oferta_descripcion,oferta_codigo,oferta_fecha_publicacion,oferta_fecha_vencimiento,oferta_limite_compra,oferta_stock_disponible,oferta_precio,oferta_precio_lista "
-                + "FROM GESTION_DE_GATOS.Oferta";// WHERE oferta_fecha_vencimiento > " + '"' +Properties.Settings.Default.fecha.ToShortDateString() + '"';
+            string query = "SELECT oferta_codigo,oferta_descripcion,oferta_fecha_publicacion,oferta_fecha_vencimiento,oferta_limite_compra,oferta_stock_disponible,oferta_precio "
+                + "FROM GESTION_DE_GATOS.Oferta WHERE '" + Properties.Settings.Default.fecha.ToShortDateString() +
+            "' BETWEEN oferta_fecha_publicacion and oferta_fecha_vencimiento";
             ConexionBD.Conexion conection = new ConexionBD.Conexion().getInstance();
             ofertas  = conection.selectReturnMultiplyRowsByQuery(query);
             grid.DataSource = ofertas;
@@ -39,8 +40,6 @@ namespace FrbaOfertas.ComprarOferta
             colEditar.Text = "Comprar";
             colEditar.Name = "";
             grid.Columns.Add(colEditar);
-
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -48,6 +47,43 @@ namespace FrbaOfertas.ComprarOferta
             this.MinimumSize = new System.Drawing.Size(this.Width, this.Height);
             this.AutoSize = true;
             this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+        }
+
+        private void grid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var senderGrid = (DataGridView)sender;
+
+            if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn &&
+                e.RowIndex >= 0 && e.RowIndex < grid.Rows.Count)
+            {
+                if (e.ColumnIndex == 0)
+                {
+                    DataRow row = (grid.CurrentRow.DataBoundItem as DataRowView).Row;
+                    DialogResult result = MessageBox.Show("¿Desea comprar la oferta de código '" + row.ItemArray[0].ToString() + "'?",
+                    "Comprar oferta",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+                    if (result == DialogResult.Yes)
+                    {
+                        realizarCompra(row);
+                    }
+                }
+            }
+        }
+
+        private bool realizarCompra(DataRow row)
+        {
+            try
+            {
+                MessageBox.Show("Compra realizada correctamente");
+                this.Hide();
+                new ComprarOfertaABM().Show();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Error al realizar la compra.\n" + e.Message);
+            }
+            return true;
         }
     }
 }
